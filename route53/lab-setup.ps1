11a1d6f94b2c9bf9e7a20e1ae6ce0fabe5f2cfbe
vagrant <ben@benpiper.com>
2018-03-17 11:04:56 -0400

    Support region-specific keys. Add db-east instance.

--- route53/lab-setup.ps1
+++ route53/lab-setup.ps1
@@ -1,13 +1,11 @@
 # Lab-Setup.ps1
 
+
 # Import VPC helper functions
 . ./vpc-functions.ps1
 
 #Import AWS credentials
 . ./credentials.ps1
-#The credentials file should contain the following two variables:
-# $AWSAccessKey="" # Your access key
-# $AWSSecretKey="" # Your secret key
 
 # Set AWS credentials
 $AWSProfileName="aws-networking-deep-dive-route-53-dns"
@@ -18,11 +16,20 @@ $amiRegionA = "ami-48351d32" #aws-elasticbeanstalk-amzn-2017.09.1.x86_64-ecs-hvm
 $AWSRegionB = "us-west-1"
 $amiRegionB = "ami-f7383a97" #aws-elasticbeanstalk-amzn-2017.09.1.x86_64-ecs-hvm-201801180451
 
+# Search for AMIs by name
+$amiName = "aws-elasticbeanstalk-amzn-2017.09.1.x86_64-ecs-hvm-2018011*"
+Get-EC2ImageByName -Region "EU-west-2" -Name $amiName | ft -Property Name,ImageId
+
 # Set your IP subnet for SSH access
 $myIP = "24.96.154.168/29"
 
 # Set the name of your SSH keypair
-$keyname = "ccnetkeypair"
+$regionAKeyname = "ccnetkeypair"
+$regionBKeyname = "ccnetkeypair"
+
+# Get existing keypairs
+Get-EC2KeyPair -Region $AWSRegionA
+Get-EC2KeyPair -Region $AWSRegionB
 
 # Set instance type
 $itype = "t2.nano"
@@ -43,11 +50,11 @@ Set-AWSCredential -ProfileName $AWSProfileName
     # Test functionality
     if ((Get-EC2Vpc).count -ge 1) { Write-Host Connectivity to AWS region $currentregion established! }
 
-    # Create webapp-vpc
+    # Create vpc
     $vpc = Create-VPC -vpcCidr "172.3.0.0/16" -vpcName "r53-lab-vpc"
 
     # Create subnet
-    $subnet = Create-Subnet -vpc $vpc -zone "us-east-1a" -name "r53-lab-1a" -IPv4Cidr "172.3.0.0/24" -IPv6Prefix "00"
+    $subnet = Create-Subnet -vpc $vpc -zone "$($AWSRegionA)a" -name "r53-lab-1a" -IPv4Cidr "172.3.0.0/24" -IPv6Prefix "00"
 
     # Create internet gateway
     $igw = Create-InternetGateway -vpc $vpc -name "r53-lab-igw"
@@ -62,7 +69,7 @@ Set-AWSCredential -ProfileName $AWSProfileName
     # Create security groups
     $sg = New-EC2SecurityGroup -VpcId $vpc.VpcId -GroupName "r53-lab-sg" -GroupDescription "r53-lab-sg"
 
-    # Create IPpermissions for public http and https
+    # Create IPpermissions for public http, https, and ssh
     $httpip = new-object Amazon.EC2.Model.IpPermission
     $httpip.IpProtocol = "tcp"
     $httpip.FromPort = 80
@@ -102,13 +109,13 @@ Set-AWSCredential -ProfileName $AWSProfileName
         $eth0.NetworkInterfaceId = $eni.NetworkInterfaceId
         $eth0.DeviceIndex = 0
         $eth0.DeleteOnTermination = $false
-        $web1 = New-EC2Instance -ImageId $amiRegionA -KeyName $keyname -InstanceType $itype -NetworkInterface $eth0
+        $web1 = New-EC2Instance -ImageId $amiRegionA -KeyName $regionAKeyname -InstanceType $itype -NetworkInterface $eth0
         New-NameTag -name "web1-east" -resourceID $web1.Instances.InstanceId
 
     ##
     ## Create web2-east instance
 
-        Create elastic network interface
+        # Create elastic network interface
         $primary = "172.3.0.20"
         $desc = "web2-east eth0"
         $eni = New-EC2NetworkInterface -SubnetId $subnet.SubnetId -Description $desc -PrivateIPAddress $primary -Group $sg
@@ -119,14 +126,35 @@ Set-AWSCredential -ProfileName $AWSProfileName
         # Associate elastic IP with web1-east eth0 ENI
         Register-EC2Address -AllocationId $eip.AllocationId -NetworkInterfaceId $eni.NetworkInterfaceId
 
-        # Create web1-east instance
+        # Create web2-east instance
         $eth0 = new-object Amazon.EC2.Model.InstanceNetworkInterfaceSpecification
         $eth0.NetworkInterfaceId = $eni.NetworkInterfaceId
         $eth0.DeviceIndex = 0
         $eth0.DeleteOnTermination = $false
-        $web2 = New-EC2Instance -ImageId $amiRegionA -KeyName $keyname -InstanceType $itype -NetworkInterface $eth0
+        $web2 = New-EC2Instance -ImageId $amiRegionA -KeyName $regionAKeyname -InstanceType $itype -NetworkInterface $eth0
         New-NameTag -name "web2-east" -resourceID $web2.Instances.InstanceId
 
+    ##
+    ## Create db-east instance
+
+        # Create elastic network interface
+        $primary = "172.3.0.100"
+        $desc = "db-east eth0"
+        $eni = New-EC2NetworkInterface -SubnetId $subnet.SubnetId -Description $desc -PrivateIPAddress $primary -Group $sg
+
+        # Allocate elastic IP
+        $eip = New-EC2Address
+
+        # Associate elastic IP with db-east eth0 ENI
+        Register-EC2Address -AllocationId $eip.AllocationId -NetworkInterfaceId $eni.NetworkInterfaceId
+
+        # Create db-east instance
+        $eth0 = new-object Amazon.EC2.Model.InstanceNetworkInterfaceSpecification
+        $eth0.NetworkInterfaceId = $eni.NetworkInterfaceId
+        $eth0.DeviceIndex = 0
+        $eth0.DeleteOnTermination = $false
+        $db = New-EC2Instance -ImageId $amiRegionA -KeyName $regionAKeyname -InstanceType $itype -NetworkInterface $eth0
+        New-NameTag -name "db-east" -resourceID $db.Instances.InstanceId
 
 ###
 ### Region B setup
@@ -138,11 +166,11 @@ Set-AWSCredential -ProfileName $AWSProfileName
     # Test functionality
     if ((Get-EC2Vpc).count -ge 1) { Write-Host Connectivity to AWS region $currentregion established! }
 
-    # Create webapp-vpc
+    # Create vpc
     $vpc = Create-VPC -vpcCidr "172.9.0.0/16" -vpcName "r53-lab-vpc"
 
     # Create subnet
-    $subnet = Create-Subnet -vpc $vpc -zone "us-west-1a" -name "r53-lab-1a" -IPv4Cidr "172.9.0.0/24" -IPv6Prefix "00"
+    $subnet = Create-Subnet -vpc $vpc -zone "$($AWSRegionB)a" -name "r53-lab-1a" -IPv4Cidr "172.9.0.0/24" -IPv6Prefix "00"
 
     # Create internet gateway
     $igw = Create-InternetGateway -vpc $vpc -name "r53-lab-igw"
@@ -197,7 +225,7 @@ Set-AWSCredential -ProfileName $AWSProfileName
         $eth0.NetworkInterfaceId = $eni.NetworkInterfaceId
         $eth0.DeviceIndex = 0
         $eth0.DeleteOnTermination = $false
-        $web1 = New-EC2Instance -ImageId $amiRegionB -KeyName $keyname -InstanceType $itype -NetworkInterface $eth0
+        $web1 = New-EC2Instance -ImageId $amiRegionB -KeyName $regionBKeyname -InstanceType $itype -NetworkInterface $eth0
         New-NameTag -name "web1-west" -resourceID $web1.Instances.InstanceId
 
     ##
@@ -219,5 +247,5 @@ Set-AWSCredential -ProfileName $AWSProfileName
         $eth0.NetworkInterfaceId = $eni.NetworkInterfaceId
         $eth0.DeviceIndex = 0
         $eth0.DeleteOnTermination = $false
-        $web2 = New-EC2Instance -ImageId $amiRegionB -KeyName $keyname -InstanceType $itype -NetworkInterface $eth0
+        $web2 = New-EC2Instance -ImageId $amiRegionB -KeyName $regionBKeyname -InstanceType $itype -NetworkInterface $eth0
         New-NameTag -name "web2-west" -resourceID $web2.Instances.InstanceId
