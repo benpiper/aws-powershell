3456384f36e9985854e114157ca2eea54d9c5b9d
vagrant <ben@benpiper.com>
2018-03-19 16:16:36 -0400

    Add get-instance.eip.ps1

---
+++ route53/get-instance-eip.ps1
@@ -0,0 +1,28 @@
+#Import AWS credentials
+. ./credentials.ps1
+
+Set-AWSCredential -AccessKey $AWSAccessKey -SecretKey $AWSSecretKey
+
+#$regions = @("us-east-1","us-west-1")
+$regions = @((Get-AWSRegion).Region)
+$urlprefix = "http://"
+
+$instances = @()
+
+$regions | ForEach-Object {
+    $regioninstances = (Get-EC2Instance -Region $_).Instances
+    if ($regioninstances -ne $null) {
+        $instances += (Get-EC2Instance -Region $_).Instances
+    }
+}
+
+$sessions = @()
+$instances | ForEach-Object {
+    $sessions += New-Object PSObject -Property @{
+        'Name'=$_.Tags.Value;
+        'IP'=$_.PublicIpAddress;
+        'URL'="$($urlprefix)$($_.PublicIpAddress)";
+    }
+}
+
+$sessions | Sort-Object -Property IP | Select-Object -Property Name,IP,URL
