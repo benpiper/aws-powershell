b110f1a6de0626ff1d67dece8d325b6098424231
vagrant <ben@benpiper.com>
2018-03-23 18:48:07 -0400

    update script

--- route53/reusable-delegation-set.ps1
+++ route53/reusable-delegation-set.ps1
@@ -5,10 +5,10 @@
 
 # Create reusable delegation set
 $delegationset = New-R53ReusableDelegationSet -CallerReference (Get-Random)
-Get-R53ReusableDelegationSets
+Get-R53ReusableDelegationSetList
 
-# View nameservers
-$delegationset.DelegationSet | Format-List
+# View nameservers only
+$delegationset.DelegationSet.NameServers
 
 # Get delegation set ID
 $dsid = $delegationset.DelegationSet.Id
@@ -22,11 +22,7 @@ $zone = New-R53HostedZone -Name $zonename -DelegationSetId $dsid -CallerReferenc
 # View zone properties
 $zone.HostedZone
 
-# Check SOA record
-nslookup -type=soa $zonename 198.6.1.5
-
-# Check NS records. The server volunteers A records.
-nslookup -type=ns $zonename 198.6.1.5
-
-# The server volunteers NS, A, and AAAA records
-nslookup -type=soa $zonename 216.52.126.33
+# View all records
+$firstns = $zone.DelegationSet.NameServers[0]
+$firstns
+nslookup -type=any $zonename $firstns
