fa2916ad1fcd9bda95e4147bb1e0f48feb254888
Ben <ben@benpiper.com>
2021-04-10 14:56:51 -0400

    update aws posh sdk install script

--- install-awspowershell.ps1
+++ install-awspowershell.ps1
@@ -1,27 +1,10 @@
 
-    Write-Host "You are using PowerShell $PSEdition!"
-
-    # Test PoSh version (core vs full) and respond accordingly
-    if ($PSEdition -eq "Core") {  
-        #PoSh Core (probably Linux but maybe bleeding edge Windows user)
+    
         if (!(Get-Module AWSPowerShell.NetCore)) { 
             Write-Host "Installing AWSPowerShell.NetCore..." 
-            Install-Module AWSPowerShell.NetCore -Scope CurrentUser -Force
+            Install-Module -name AWSPowerShell.NetCore -Scope CurrentUser -Force -AllowClobber
         }
         Write-Host "Importing module..."
         Import-Module AWSPowerShell.NetCore -Force
-        # If using PoSh Core on windows, add: Install-Module AWSPowerShell -SkipPublisherCheck
-    } elseif ($PSEdition -eq "Desktop")
-    {
-        #PoSh Windows
-        if ((Get-Module AWSPowerShell) -notlike "AWSPowerShell" ) {
-        Write-Host "Installing AWSPowerShell..."
-        Install-Module AWSPowerShell -Scope CurrentUser -Force
-        }
-        Write-Host "Importing module..."
-        Import-Module AWSPowerShell -Force
-    }
-
-    #NB: Do not install Core module in regular Windows PowerShell! It's not the same thing and won't work.
-
+ 
     Get-AWSPowerShellVersion
