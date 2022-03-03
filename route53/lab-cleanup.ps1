41f3d12dc6587d773fbb01e7bed629734d07256b
Ben Piper <ben@benpiper.com>
2022-03-03 00:14:10 -0500

    update

--- route53/lab-cleanup.ps1
+++ route53/lab-cleanup.ps1
@@ -9,9 +9,28 @@ $instances = Get-EC2Instance
 foreach ($instance in $instances) { Remove-EC2Instance $instances.Instances.InstanceId -Force }
 Write-Host Proceed when instances are terminated
 pause
+# Remove network interfaces
 # Remove VPCs
-$vpc = Get-EC2Vpc -Region $region -Filter @( @{name="cidr";value=$cidr})
-Remove-EC2Vpc -VpcId $vpc.VpcId -Force
+$vpcs = Get-EC2Vpc -Region $region -Filter @( @{name="cidr";value=$cidr})
+foreach ($vpc in $vpcs) { Remove-EC2Vpc -VpcId $vpc.VpcId -Force }
+# Remove resource record sets
+# Remove zones
+# Remove reusable delegation sets
+
+
+$cidr = "172.3.0.0/16"
+$region = "us-east-1"
+
+Set-DefaultAWSRegion $region
+# Remove instances
+$instances = Get-EC2Instance
+foreach ($instance in $instances) { Remove-EC2Instance $instances.Instances.InstanceId -Force }
+Write-Host Proceed when instances are terminated
+pause
+# Remove network interfaces
+# Remove VPCs
+$vpcs = Get-EC2Vpc -Region $region -Filter @( @{name="cidr";value=$cidr})
+foreach ($vpc in $vpcs) { Remove-EC2Vpc -VpcId $vpc.VpcId -Force }
 # Remove resource record sets
 # Remove zones
 # Remove reusable delegation sets
